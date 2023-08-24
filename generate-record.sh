b9d399dfe3b483b1447124fd0658aae917b93d5f
sorawitlertumpaisakulwong <sorawit@senestia.com>
2023-08-24 20:15:48 +0700

    add script gen data

--- generate-record.sh
+++ generate-record.sh
@@ -4,18 +4,33 @@
 MYSQL_USER="root"
 MYSQL_PASSWORD="root"
 DATABASE="my_database"
-TABLE="users"
+
+n_bash=1000
 
 # Number of records to insert
-N=1000000
+num_records=1000
 
-# Loop to insert records
-for ((i = 0; i < N; i++)); do
-    username="user${i}"
-    email="user${i}@example.com"
-    birthdate="1990-01-01"
-    desctiprion="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+# Loop to generate values for the insert statement
+# shellcheck disable=SC2167
+for ((x = 0; x < n_bash; x++)); do
+    # Prepare the insert statement
+    insert_query="INSERT INTO users (username, email, birthdate, desctiprion) VALUES "
 
-    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${DATABASE}" -e "INSERT INTO ${TABLE} (username, email, birthdate, desctiprion) VALUES ('${username}', '${email}', '${birthdate}', '${desctiprion}')"
-    echo "Inserted record ${i}"
+    # shellcheck disable=SC2165
+    for i in $(seq $num_records); do
+        username="user${i}"
+        email="user${i}@example.com"
+        birthdate="1990-01-01"
+        desctiprion="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+        # Append values to the insert statement
+        insert_query+="('$username', '$email', '$birthdate', '$desctiprion'),"
+    done
+
+    # Remove the trailing comma
+    insert_query="${insert_query%,}"
+
+    # Use the mysql command to execute the insert statement
+    mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$DATABASE" -e "$insert_query;"
+    echo "Insert bash $x $num_records records"
 done
